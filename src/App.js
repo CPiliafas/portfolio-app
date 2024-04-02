bc5e02dc30f50eaf78729054c52cc83b53bc4655
CPiliafas <PJRc3160@gmail.com>
2024-04-02 14:44:51 -0400

    Fixed images and added more content to the page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import gc1 from './images/gc1.png';
+import gc2 from './images/gc2.png';
+import mkx1 from './images/mkx1.png';
+import mkx2 from './images/mkx2.png';
+import mkx3 from './images/mkx3.png';
+import mkx4 from './images/mkx4.png';
+import mkx5 from './images/mkx5.png';
 
 function App() {
   return (
@@ -49,50 +55,69 @@ function App() {
               <ol><a href="https://www.dmeinterns.org/web-development/">Web Development</a></ol>
               <ol><a href="https://www.dmeinterns.org/social-media/">Social Media</a></ol>
             <h2>U.S. Embassy of Tokyo Daily Trade Site (private, hosted on SquareSpace)</h2>
+              <ol>During this internship, I was given the opportuniity to help create a website through SquareSpace that displayed and archived various Daily Trade Newsletter articles for the U.S. Embassy of Tokyo, Japan. Alongside another web developer intern, the two of us designed, updated, and managed the website. We also wrote documentation for updating and managing the website for when we left. We also formatted various articles near daily during the course of the internship.</ol>
+            <h2>Grand Traverse Internists Contract Work</h2>
+              <ol>Over time, I have helped change aspects of the site's visuals. Such as layout, fonts, images, etc., while also testing functionality, accessibility, and SEO.</ol>
+              <ol><a href="https://gtinternists.com/">Grand Traverse Internists Website</a></ol>
             <ol><a href="https://github.com/CPiliafas">Github Repository</a></ol>
             <br></br>
             <h2>Some Example Work + Links to GitHub Repositories</h2>
             <ol><a href="https://github.com/CPiliafas/GalacticCafe">Galactic Cafe Page</a></ol>
-            <img src={gc1} alt="Galactic Cafe Image 1" width="600" height="400"/>
+            <img src={gc1} alt="Galactic Cafe Image 1" width="900px" height="600px"/>
+            <br />
+            <img src={gc2} alt="Galactic Cafe Image 2" width="900px" height="600px"/>
             <p>This project was for me to begin learning React.js as a framework as well as practicing with media queries. I wanted to create a mock up of a cafe business' web page. Through this project I was able to learn the basics of React and how much I enjoy the framework and wish to use it in the future.</p>
-            <br></br>
+            <br />
             <ol><a href="https://github.com/CPiliafas/MKXCompendium">Mortal Kombat X Fan Page</a></ol>
             <div class="responsive">
             <div class="gallery">
               <a target="_blank" href="/images/mkx1.png">
-                <img src="/images/mkx1.png" alt="Mortal Kombat X Index Page" width="600" height="400" />
+                <img src={mkx1} alt="Mortal Kombat X Index Page" width="800" height="500"/>
               </a>
-              <div class="desc">Add a description of the image here</div>
+              <div class="desc">This index page delivers a brief paragraph about the video game Mortal Kombat X. On the right half of the page is a grid featuring each of the playable fighters available in the game. Clicking one takes you to that character's dedidcated page.</div>
             </div>
           </div>
-
-
-        <div class="responsive">
-          <div class="gallery">
-            <a target="_blank" href="img_forest.jpg">
-              <img src="img_forest.jpg" alt="Forest" width="600" height="400" />
-            </a>
-            <div class="desc">Add a description of the image here</div>
-          </div>
-        </div>
+          <br />
 
         <div class="responsive">
           <div class="gallery">
-            <a target="_blank" href="img_lights.jpg">
-              <img src="img_lights.jpg" alt="Northern Lights" width="600" height="400" />
+            <a target="_blank" href="./images/mkx2.png">
+              <img src={mkx2} alt="Sub-Zero Character Info Page" width="800" height="500" />
             </a>
-            <div class="desc">Add a description of the image here</div>
+            <div class="desc">Each character page features information about them.</div>
           </div>
         </div>
+        <br />
 
         <div class="responsive">
           <div class="gallery">
-            <a target="_blank" href="img_mountains.jpg">
-              <img src="img_mountains.jpg" alt="Mountains" width="600" height="400" />
+            <a target="_blank" href="./images/mkx3.png">
+              <img src={mkx3} alt="Sub-Zero Character Move List Section" width="800" height="500" />
             </a>
-            <div class="desc">Add a description of the image here</div>
+            <div class="desc">Each character page features gameplay information such as their special moves and the inputs players need to do to use them.</div>
           </div>
         </div>
+        <br />
+
+        <div class="responsive">
+          <div class="gallery">
+            <a target="_blank" href="./images/mkx4.png">
+              <img src={mkx4} alt="Mortal Kombat X About Page" width="800" height="500" />
+            </a>
+            <div class="desc">The About page dives a little more into why I made the project as well as listing out resources I used.</div>
+          </div>
+        </div>
+        <br />
+
+        <div class="responsive">
+          <div class="gallery">
+            <a target="_blank" href="./images/mkx5.png">
+              <img src={mkx5} alt="Gameplay Information Page" width="800" height="500" />
+            </a>
+            <div class="desc">The Gameplay Info page dives more into the mechanics and features of Mortal Kombat X.</div>
+          </div>
+        </div>
+        <br />
             <p>This project was more for fun and to put some of my personality and interests into my work, something I really enjoy doing. I wanted to create a site of sorts with multiple links and different layouts of content. I was inspired by similar styled web pages for other video games I enjoy and wanted to try creating something similar and I'm satisfied with how it turned out.</p>
           </article>
             </div>
